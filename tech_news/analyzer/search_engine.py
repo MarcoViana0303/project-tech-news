49ff37f13dd52d39a9e9af97f54734091cd45a19
Marco Viana <marcoviana100@gmail.com>
2023-09-28 00:34:16 -0300

    feat: Implementada a função search_by_title, responsável por fazer buscas por título

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,15 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # Realiza a busca no banco de dados por título (case insensitive)
+    results = db.news.find({"title": {"$regex": title, "$options": "i"}})
+
+    # Transforma os resultados em uma lista de tuplas (título, URL)
+    news_list = [(result["title"], result["url"]) for result in results]
+
+    return news_list
 
 
 # Requisito 8
