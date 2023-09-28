afc3fa8e8a6c92ffb914b2750bf6ec639bb09545
Marco Viana <marcoviana100@gmail.com>
2023-09-28 01:41:17 -0300

    feat: Implementada a função top_5_categories, responsável por listar as cinco categorias com maior ocorrencia no banco de dados

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,30 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # Obtém a coleção de notícias do banco de dados
+    news_collection = db.get_collection("news")
+
+    # Define uma sequência de operações de agregação
+    # para calcular as categorias mais populares
+    pipeline = [
+        # Separa cada categoria em documentos individuais
+        {"$unwind": "$category"},
+        # Agrupa as categorias e conta o número de ocorrências
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        # Ordena as categorias por contagem decrescente e,
+        # em caso de empate, por ordem alfabética
+        {"$sort": {"count": -1, "_id": 1}},
+        # Limita o resultado às cinco categorias mais populares
+        {"$limit": 5},
+    ]
+
+    # Executa as operações de agregação e armazena os resultados em uma lista
+    results = list(news_collection.aggregate(pipeline))
+
+    # Extrai apenas os nomes das categorias dos resultados
+    top_categories = [result["_id"] for result in results]
+
+    # Retorna as cinco categorias mais populares
+    return top_categories
