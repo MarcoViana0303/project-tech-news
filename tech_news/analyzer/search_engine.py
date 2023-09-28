a45cdf9847e3d0b1464c85da695dcb50770da4c4
Marco Viana <marcoviana100@gmail.com>
2023-09-28 00:38:41 -0300

    feat: Implementada a função search_by_date, responsável por buscar notícias do banco de dados por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 # Requisito 7
@@ -14,8 +15,22 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        # Converte a data para o formato dd/mm/AAAA
+        formatted_date = (
+            datetime.strptime(date, "%Y-%m-%d")
+            .strftime("%d/%m/%Y")
+        )
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    # Realiza a busca no banco de dados por data
+    results = db.news.find({"timestamp": formatted_date})
+
+    # Transforma os resultados em uma lista de tuplas (título, URL)
+    news_list = [(result["title"], result["url"]) for result in results]
+
+    return news_list
 
 
 # Requisito 9
